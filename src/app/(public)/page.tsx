41c3df89b8ad91ff361eb5652ccee8ee9d04231c
renanvilelati <renanvilelati@gmail.com>
2026-03-18 12:04:29 -0300

    feat: add footer

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,11 @@
 import { HeroSession } from './_components/hero-section';
 import { Header } from '../../layout/header';
 import { ProfessionalsSection } from './_components/professionals-section';
+import { Footer } from '@/layout/footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Header />
 
       <main>
@@ -12,6 +13,8 @@ export default function Home() {
 
         <ProfessionalsSection />
       </main>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/layout/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="py-6 text-center text-sm text-gray-500 md:text-base">
+      <p>Todos os direitos reservados {new Date().getFullYear()} - @dentmed</p>
+    </footer>
+  );
+};
