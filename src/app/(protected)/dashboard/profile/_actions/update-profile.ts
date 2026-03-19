0d8ceca28719819850357e82827d812c3dd98670
renanvilelati <renanvilelati@gmail.com>
2026-03-19 18:51:12 -0300

    feat: create update profile action

---
+++ src/app/(protected)/dashboard/profile/_actions/update-profile.ts
@@ -0,0 +1,51 @@
+'use server';
+
+import { auth } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import { profileSchema, TProfileFormData } from '../_schemas/profile-form';
+import { revalidatePath } from 'next/cache';
+
+export const updateProfile = async (formData: TProfileFormData) => {
+  const session = await auth();
+
+  if (!session?.user.id) {
+    return {
+      error: 'Usuário não encontrado',
+    };
+  }
+
+  const schema = profileSchema.safeParse(formData);
+
+  if (!schema.success) {
+    return {
+      error: 'Preencha todos os campos',
+    };
+  }
+
+  try {
+    await prisma.user.update({
+      where: {
+        id: session.user.id,
+      },
+      data: {
+        name: formData.name,
+        address: formData.address,
+        phone: formData.phone,
+        status: formData.status,
+        timeZone: formData.timeZone,
+        times: formData.times,
+      },
+    });
+
+    revalidatePath('/dashboard/profile');
+
+    return {
+      message: 'Clínica atualizada com sucesso!',
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      error: 'Falha ao atualizar clínica',
+    };
+  }
+};
