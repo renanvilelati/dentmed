c55ff700bea4d02b2e8658ff8dbd9f6b57b2c6fd
renanvilelati <renanvilelati@gmail.com>
2026-03-21 12:14:55 -0300

    feat: create disable service action

---
+++ src/app/(protected)/dashboard/services/_actions/disable-service.ts
@@ -0,0 +1,53 @@
+'use server';
+
+import { auth } from '@/lib/auth';
+import { prisma } from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+import z from 'zod';
+
+const formSchema = z.object({
+  serviceId: z.string().min(1),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+export const disableService = async (formData: FormSchema) => {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    return {
+      error: 'Falha ao deletar serviço',
+    };
+  }
+
+  const parsedSchema = formSchema.safeParse(formData);
+
+  if (!parsedSchema.success) {
+    return {
+      error: parsedSchema.error.issues[0].message,
+    };
+  }
+
+  try {
+    await prisma.service.update({
+      where: {
+        id: formData.serviceId,
+        userId: session.user.id,
+      },
+      data: {
+        status: false,
+      },
+    });
+
+    revalidatePath('/dashboard/services');
+
+    return {
+      message: 'Serviço deletado com sucesso!',
+    };
+  } catch (error) {
+    console.error('Erro ao deletar serviço:', error);
+    return {
+      error: 'Falha ao deletar serviço',
+    };
+  }
+};
