fd41c58e51cd3c51eb7eee32e85183ee07a59f86
renanvilelati <renanvilelati@gmail.com>
2026-03-30 13:10:16 -0300

    feat: add appointment button to dashboard

---
+++ src/app/(protected)/dashboard/components/button-copy.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { LinkIcon } from 'lucide-react';
+import { toast } from 'sonner';
+
+const ButtonCopy = ({ userId }: { userId: string }) => {
+  const handleCopyLink = async () => {
+    await navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_API_URL}/clinic/${userId}`,
+    );
+
+    toast.success('Link da clínica copiado');
+  };
+
+  return (
+    <Button
+      className="bg-gray-300 text-black hover:bg-gray-200"
+      onClick={handleCopyLink}
+    >
+      <LinkIcon className="h-5 w-5" />
+    </Button>
+  );
+};
+
+export default ButtonCopy;
--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from '@/components/ui/button';
 import { auth } from '@/lib/auth';
+import { Calendar } from 'lucide-react';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import ButtonCopy from './components/button-copy';
 
 const DashboardPage = async () => {
   const session = await auth();
@@ -9,12 +13,19 @@ const DashboardPage = async () => {
   }
 
   return (
-    <div>
-      <h2>Dashboard Page</h2>
-      <div className="h-[600px] w-full bg-gray-500">1</div>
-      <div className="h-[600px] w-full bg-gray-300">2</div>
-      <div className="h-[600px] w-full bg-gray-200">3</div>
-    </div>
+    <main>
+      <div className="flex items-center justify-end space-x-2">
+        <Link href={`/clinic/${session.user.id}`} target="_blank">
+          <Button className="flex-1 bg-orange-500 hover:bg-orange-400 md:flex-0">
+            <Calendar className="h-5 w-5" />
+            <span>Novo agendamento</span>
+          </Button>
+        </Link>
+        <ButtonCopy userId={session.user.id} />
+      </div>
+
+      <section className="mt-4 grid grid-cols-1 gap-4 lg:grid-cols-2"></section>
+    </main>
   );
 };
 
