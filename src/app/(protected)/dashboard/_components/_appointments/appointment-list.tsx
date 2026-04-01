7a2754a1803e87e1f749b776e1de2556d64ddf47
renanvilelati <renanvilelati@gmail.com>
2026-04-01 13:27:52 -0300

    feat: add dialog for details appointment

--- src/app/(protected)/dashboard/_components/_appointments/appointment-list.tsx
+++ src/app/(protected)/dashboard/_components/_appointments/appointment-list.tsx
@@ -11,8 +11,11 @@ import { Eye, X } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { cancelAppointment } from '../../actions/cancel-appointment';
 import { toast } from 'sonner';
+import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { useState } from 'react';
+import DialogAppointment from './dialog-appointment';
 
-type TAppointmentWIthService = Prisma.AppointmentGetPayload<{
+export type TAppointmentWIthService = Prisma.AppointmentGetPayload<{
   include: {
     service: true;
   };
@@ -23,6 +26,9 @@ type AppointmentListProps = {
 };
 
 const AppointmentList = ({ times }: AppointmentListProps) => {
+  const [open, setOpen] = useState(false);
+  const [selectedAppointment, setSelectedAppointment] =
+    useState<TAppointmentWIthService | null>(null);
   const queryClient = useQueryClient();
   const searchParams = useSearchParams();
 
@@ -94,64 +100,71 @@ const AppointmentList = ({ times }: AppointmentListProps) => {
   };
 
   return (
-    <Card>
-      <CardHeader className="flex items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-xl text-gray-500 md:text-2xl">
-          Agendamentos
-        </CardTitle>
-        <AddPlusButton onCLick={handleClick} />
-      </CardHeader>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <Card>
+        <CardHeader className="flex items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-xl text-gray-500 md:text-2xl">
+            Agendamentos
+          </CardTitle>
+          <AddPlusButton onCLick={handleClick} />
+        </CardHeader>
 
-      <CardContent>
-        <ScrollArea className="h-340 w-full flex-1 pr-0 lg:max-h-[calc(100vh-15rem)]">
-          {isLoading ? (
-            <p>Carregando...</p>
-          ) : (
-            times.map((item) => {
-              const ocuppant = occupantMap[item];
+        <CardContent>
+          <ScrollArea className="h-340 w-full flex-1 pr-0 lg:max-h-[calc(100vh-15rem)]">
+            {isLoading ? (
+              <p>Carregando...</p>
+            ) : (
+              times.map((item) => {
+                const ocuppant = occupantMap[item];
 
-              if (ocuppant) {
-                return (
-                  <div key={item} className="flex">
-                    <div className="mb-1 flex w-full items-center gap-2 p-1 pl-0">
-                      <p>{item}</p>
-                      <div className="ml-3 flex w-full gap-4 rounded bg-orange-100 p-2">
-                        <p>{ocuppant.name}</p> {' - '}
-                        <p>{ocuppant.phone}</p>
-                      </div>
-                      <div className="flex gap-2">
-                        <Button
-                          size={'sm'}
-                          className="bg-blue-400 hover:bg-blue-500"
-                        >
-                          <Eye />
-                        </Button>
-                        <Button
-                          size={'sm'}
-                          className="bg-red-400 hover:bg-red-500"
-                          onClick={() => handleCancelAppointment(ocuppant.id)}
-                        >
-                          <X />
-                        </Button>
+                if (ocuppant) {
+                  return (
+                    <div key={item} className="flex">
+                      <div className="mb-1 flex w-full items-center gap-2 p-1 pl-0">
+                        <p>{item}</p>
+                        <div className="ml-3 flex w-full gap-4 rounded bg-orange-100 p-2">
+                          <p>{ocuppant.name}</p> {' - '}
+                          <p>{ocuppant.phone}</p>
+                        </div>
+                        <div className="flex gap-2">
+                          <DialogTrigger asChild>
+                            <Button
+                              size={'sm'}
+                              className="bg-blue-400 hover:bg-blue-500"
+                              onClick={() => setSelectedAppointment(ocuppant)}
+                            >
+                              <Eye />
+                            </Button>
+                          </DialogTrigger>
+                          <Button
+                            size={'sm'}
+                            className="bg-red-400 hover:bg-red-500"
+                            onClick={() => handleCancelAppointment(ocuppant.id)}
+                          >
+                            <X />
+                          </Button>
+                        </div>
                       </div>
                     </div>
+                  );
+                }
+
+                return (
+                  <div key={item} className="mb-2 flex items-center gap-2">
+                    <p className="min-w-12">{item}</p>
+                    <p className="rounded bg-emerald-500 p-1.5 text-white">
+                      Disponível
+                    </p>
                   </div>
                 );
-              }
+              })
+            )}
+          </ScrollArea>
+        </CardContent>
+      </Card>
 
-              return (
-                <div key={item} className="mb-2 flex items-center gap-2">
-                  <p className="min-w-12">{item}</p>
-                  <p className="rounded bg-emerald-500 p-1.5 text-white">
-                    Disponível
-                  </p>
-                </div>
-              );
-            })
-          )}
-        </ScrollArea>
-      </CardContent>
-    </Card>
+      <DialogAppointment appointment={selectedAppointment} />
+    </Dialog>
   );
 };
 
---
+++ src/app/(protected)/dashboard/_components/_appointments/dialog-appointment.tsx
@@ -0,0 +1,81 @@
+import {
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+} from '@/components/ui/dialog';
+import { TAppointmentWIthService } from './appointment-list';
+import { formatCurrency } from '@/utils/formatCurrency';
+import { formatDuration } from '@/utils/formatDuration';
+import { format } from 'date-fns';
+import { Separator } from '@/components/ui/separator';
+
+type DialogAppointmentProps = {
+  appointment: TAppointmentWIthService | null;
+};
+
+const DialogAppointment = ({ appointment }: DialogAppointmentProps) => {
+  console.log(appointment);
+
+  return (
+    <DialogContent className="gap-2">
+      <DialogHeader>
+        <DialogTitle className="text-sm text-gray-700 md:text-xl">
+          Detalhes do Agendamento
+        </DialogTitle>
+      </DialogHeader>
+      <DialogDescription className="p-0">
+        Veja todos os detalhes do agendamento selecionado.
+      </DialogDescription>
+
+      <div className="py-2">
+        {appointment && (
+          <article className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl font-semibold text-gray-500">Serviço</h3>
+              <p>
+                <span className="font-semibold">Serviço</span>:{' '}
+                {appointment.service.name}
+              </p>
+              <p>
+                <span className="font-semibold">Data</span>:{' '}
+                {format(appointment.appointmentDate, 'dd/MM/yyyy')}
+              </p>
+              <p>
+                <span className="font-semibold">Preço</span>:{' '}
+                {formatCurrency(appointment.service.price / 100)}
+              </p>
+              <p>
+                <span className="font-semibold">Duração</span>:{' '}
+                {formatDuration(appointment.service.duration)}
+              </p>
+            </div>
+            <Separator />
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl font-semibold text-gray-500">
+                Dados do cliente
+              </h3>
+              <p>
+                <span className="font-semibold">Nome</span>: {appointment.name}
+              </p>
+              <p>
+                <span className="font-semibold">Email</span>:{' '}
+                {appointment.email}
+              </p>
+              <p>
+                <span className="font-semibold">Telefone</span>:{' '}
+                {appointment.phone}
+              </p>
+              <p>
+                <span className="font-semibold">Horário</span>:{' '}
+                {appointment.time}
+              </p>
+            </div>
+          </article>
+        )}
+      </div>
+    </DialogContent>
+  );
+};
+
+export default DialogAppointment;
