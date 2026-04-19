6d68c891c9545d496152fca0f7d44e8a31c9c55b
renanvilelati <renanvilelati@gmail.com>
2026-04-18 22:42:37 -0300

    feat: implement user authentication forms with sign-up and login functionality

--- src/features/authentication/schemas/signup.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-
-export const signupSchema = z.object({
-  email: z.email('Digite um e-mail válido'),
-  password: z.string().min(1, 'A senha é obrigatória'),
-  name: z.string().min(1, 'O nome é obrigatório'),
-});
-
-export type TsignupSchema = z.infer<typeof signupSchema>;
