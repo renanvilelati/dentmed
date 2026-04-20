d7f6b4a5bfd6016b0ddc06984c07accea2a8fed9
renanvilelati <renanvilelati@gmail.com>
2026-04-19 22:26:31 -0300

    feat: add stripe integration for server and client side

---
+++ src/shared/lib/stripe/stripe-js.ts
@@ -0,0 +1,8 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+export const getStripeJs = async () => {
+  const stripeJs = await loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+  );
+  return stripeJs;
+};
